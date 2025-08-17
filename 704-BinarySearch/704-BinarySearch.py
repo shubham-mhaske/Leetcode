734291fe3893b8b670d2113f67f9212090bb0c6d
shubham-mhaske <49318407+shubham-mhaske@users.noreply.github.com>
2025-08-17 17:02:55 -0500

    File updated at 8/17/2025, 5:02:55 PM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,9 +1,10 @@
-# Last updated: 8/17/2025, 4:32:09 PM
+# Last updated: 8/17/2025, 5:02:55 PM
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        
         start = 0
         end = len(nums)-1
-
+        '''
         while start<=end:
             mid = int(start + (end-start)/2)
             if nums[mid] == target:
@@ -11,5 +12,21 @@ class Solution:
             if nums[mid]> target:
                 end = mid-1
             else:
-                start = start+1
+                start = mid+1
         return -1
+        '''
+
+        #recursive
+        return self.recursive_search(nums,target,start,end)
+
+    def recursive_search(self,nums:List[int],target:int,start:int,end:int) -> int:
+        mid = int(start+ (end-start)/2)
+        if start>end:
+            return -1
+        if nums[mid] == target:
+            return mid
+        if target<nums[mid]:
+            end = mid-1
+        else:
+            start = mid+1
+        return self.recursive_search(nums,target,start,end)
